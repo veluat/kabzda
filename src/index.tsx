7807717b5aa0c5b981c22ce7c55a81d73cf99e27
Julia <veluat@gmail.com>
2022-09-10 17:21:19 +0300

    First commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+/*let a = {
+    name: 'Julia',
+    age: 18,
+    address: {
+        city: 'Minsk',
+        country: 'Belarus'
+    }
+};
+
+let users = [
+    {
+        name: 'Julia',
+        age: 18,
+        address: {
+            city: 'Minsk',
+            country: 'Belarus'
+        }
+    },
+    {
+        name: 'Oleg',
+        age: 25,
+        address: {
+            city: 'Minsk',
+            country: 'Belarus'
+        }
+    },
+]
+
+console.log(users[0].address.country);
+
+*/
+
+
